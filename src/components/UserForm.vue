318be2b0c763152149e8876fbbe5ecc5c54036de
YXY1017 <jyii0007@student.monash.edu>
2024-08-08 11:18:42 +1000

    week3 lab setup, add UserForm to the library web.

---
+++ src/components/UserForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>User Information Form / Credentials</h1>
+  </div>
+</template>
+
+<script setup>
+// Our logic will go here
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+  font-size: 24px;
+}
+</style>
